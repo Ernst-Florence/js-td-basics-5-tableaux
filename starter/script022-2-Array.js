5f45b56d4dd8f60c9df41d359771aa194b930dbc
florenceernst <florence.ernst@icloud.com>
2023-11-15 13:10:40 +0100

    Initial Commit

--- starter/script022-2-Array.js
+++ starter/script022-2-Array.js
@@ -1,6 +1,6 @@
 /*****************************
-* 022 - Les tableaux indicés (Arrays)
-*/
+ * 022 - Les tableaux indicés (Arrays)
+ */
 
 
 // 8. Tableau à deux dimensions
@@ -11,33 +11,44 @@ On parle alors de tableau à deux dimensions.
 Voici un exemple de tableau à deux dimensions (un tableau de tableaux),
 qui pourrait être utilisé comme tableau de positionnement des mines d'un jeu du démineur.
 */
-const mines = [[0, 1, 0, 1],
-             [1, 0, 0, 0], 
-             [1, 0, 1, 0], 
-             [0, 0, 0, 0]];
-/* 
+var mines = [[0, 1, 0, 1],
+    [1, 0, 0, 0],
+    [1, 0, 1, 0],
+    [0, 0, 0, 0]];
+/*
 EXERCICE :
 - évaluez chacune des expressions qui suivent
 - vérifiez votre réponse en affichant la valeur de l'expression dans la console
 */
 mines		// …………………………………………………………………
+// [[0, 1, 0, 1],
+// [1, 0, 0, 0],
+// [1, 0, 1, 0],
+// [0, 0, 0, 0]]
 mines[1]	// …………………………………………………………………
+// [1, 0, 0, 0],
 mines[0][3] // …………………………………………………………………
+//0
+
+/*
+EXERCICE 7 :
+- Etape 1 :
+	- récupérez la valeur qui se trouve à la deuxième ligne troisième colonne du tableau
+	- si cette valeur est 1, affichez "Pas de chance, vous avez explosé !",
+	sinon affichez "Vous avez de la chance, cet emplacement ne contient pas de mine"
+	- testez de la même façon l'emplacement situé en première ligne quatrième colonne
+- Etape 2 :
+	- indiquez combien de mines contient
+		- la première ligne de ce tableau
+		- le tableau en entier
+*/
+
+mines[1][2] === 0 ? console.log("Vous avez de la chance, cet emplacement ne contient pas de mine") : console.log("pas de chance vous avez explosé");
+
+mines[0][3] === 0 ? console.log("Vous avez de la chance, cet emplacement ne contient pas de mine") : console.log("pas de chance vous avez explosé");
+
+console.log(mines[0].indexOf())
 
 
 
-
-// EXERCICE 7 :
-// Etape 1 :
-// Pour récupérer la valeur à la deuxième ligne troisième colonne du tableau,
-// utilisez les indices corrects (n'oubliez pas que les indices commencent à zéro).
-
-
-
-// Testez de la même manière l'emplacement situé en première ligne quatrième colonne
-
-
-// Etape 2 :
-// Pour indiquer combien de mines contient la première ligne de ce tableau,
-// vous devez parcourir les éléments de cette ligne et compter combien d'entre eux sont égaux à 1.
 
--- starter/tableaux-p2-somme.js
+++ starter/tableaux-p2-somme.js
@@ -1,20 +1,55 @@
 /*
 TABLEAUX - PRÉPA 2 : Somme des valeurs
-1. Initialisez un tableau qui contient les nombres 11, 3, 7, 2, 9 et 10.
-2. Affichez le tableau (précédé du texte : "Voici mon tableau de nombres :").
-3. Calculez puis affichez la somme des valeurs du tableau 'nombres' :
-   - Utilisez la méthode 'forEach()' pour afficher la somme des nombres dans le tableau (précédée du texte : "La somme des nombres de mon tableau est …").
-   - Utilisez une boucle 'for/of' pour faire la même chose.
+1. Initialisez un tableau qui contient les nombres 11, 3, 7, 2, 9 et 10 ;
+2. Affichez-le (précédé du texte : "Voici mon tableau de nombres :") ;
+3. Calculez puis affichez la somme des valeurs du tableau nombres
+	(précédé du texte : "La somme des nombres de mon tableau est …")
+	1°) à l'aide de la méthode froEach()
+	2°) en utilisant la boucle for/of
+
+*/
+
+const tableaux = [3, 11, 7, 2, 9, 10];
+console.log(`voici mon tableau de nombres : ${tableaux}`);
+
+let max = 0;
+tableaux.forEach(nombre => (max += nombre))
+
+console.log(max);
+/*
 
 Version alternative (avec BONUS) :
 1. Initialisez
-   - une variable 'message' à une chaîne vide ;
-   - une variable 'somme' à 0.
-2. Parcourez votre tableau de nombres à l'aide d'une boucle 'for' pour construire le message : "… + … + … = ".
-3. Parcourez votre tableau de nombres à l'aide d'une boucle 'for/of' pour calculer la somme des valeurs du tableau.
-4. Affichez le message final : "… + … + … = …".
+	- une variable message à une chaîne vide ;
+	- une variable somme à 0 ;
+2. Parcourez votre tableau de nombres à l'aide d'une boucle for pour
+	construitre le message : "… + … + … = " ;
+3. Parcourez votre tableau de nombres à l'aide d'une boucle for-of pour
+	calculer la somme des valeurs du tableau
+4. Affichez le message final : "… + … + … = …"
 */
 
+// Solution avec la méthode forEach()
+
+
+
+
+
+
+
+
+
+// Solution avec la méthode for/of
+
+
+
+
+
+
+
+
+// *******************************
+// Solution avec BONUS
 
 
 
